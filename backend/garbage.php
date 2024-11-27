919eb766e4cc745a5adcadaa071bc8a0fdd1966f
elwanm <98734893+elwanm@users.noreply.github.com>
2024-11-27 16:32:01 +0200

    Update garbage.php

--- backend/garbage.php
+++ backend/garbage.php
@@ -2,15 +2,13 @@
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
+
 echo json_encode(["status" => "Garbage cleanup completed successfully."]);
-// Disable Compression
+
 @ini_set('zlib.output_compression', 'Off');
 @ini_set('output_buffering', 'Off');
 @ini_set('output_handler', '');
 
-/**
- * @return int
- */
 function getChunkCount()
 {
     if (
@@ -18,49 +16,44 @@ function getChunkCount()
         || !ctype_digit($_GET['ckSize'])
         || (int) $_GET['ckSize'] <= 0
     ) {
-        return 4;
+        return 4; // Default chunk size if none specified
     }
 
     if ((int) $_GET['ckSize'] > 1024) {
-        return 1024;
+        return 1024; // Max chunk size
     }
 
-    return (int) $_GET['ckSize'];
+    return (int) $_GET['ckSize']; // Return the chunk size from the query parameter
 }
 
-/**
- * @return void
- */
 function sendHeaders()
 {
-    header('HTTP/1.1 200 OK');
+    header('HTTP/1.1 200 OK'); // Response OK header
 
     if (isset($_GET['cors'])) {
         header('Access-Control-Allow-Origin: *');
         header('Access-Control-Allow-Methods: GET, POST');
     }
 
-    // Indicate a file download
     header('Content-Description: File Transfer');
     header('Content-Type: application/octet-stream');
     header('Content-Disposition: attachment; filename=random.dat');
     header('Content-Transfer-Encoding: binary');
-
-    // Cache settings: never cache this request
     header('Cache-Control: no-store, no-cache, must-revalidate, max-age=0, s-maxage=0');
     header('Cache-Control: post-check=0, pre-check=0', false);
     header('Pragma: no-cache');
 }
 
-// Determine how much data we should send
 $chunks = getChunkCount();
 
-// Generate data
 $data = openssl_random_pseudo_bytes(1048576);
 
-// Deliver chunks of 1048576 bytes
 sendHeaders();
+
 for ($i = 0; $i < $chunks; $i++) {
     echo $data;
-    flush();
+    flush(); // Send output immediately
 }
+
+
+?>
