89fcf8b53b213d402c8ea8f78eca2aa5f561b01a
elwanm <98734893+elwanm@users.noreply.github.com>
2024-11-28 19:55:20 +0200

    Add files via upload

--- backend/empty.php
+++ backend/empty.php
@@ -1,16 +1,16 @@
 <?php
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
-header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
-header("Access-Control-Allow-Credentials: true");
-
-if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-    header('HTTP/1.1 204 No Content');
-    exit(0);
-}
 
 header('HTTP/1.1 200 OK');
-echo json_encode(["status" => "Cleanup completed successfully."]);
-exit;
-?>
 
+if (isset($_GET['cors'])) {
+    header('Access-Control-Allow-Origin: *');
+    header('Access-Control-Allow-Methods: GET, POST');
+    header('Access-Control-Allow-Headers: Content-Encoding, Content-Type');
+}
+
+header('Cache-Control: no-store, no-cache, must-revalidate, max-age=0, s-maxage=0');
+header('Cache-Control: post-check=0, pre-check=0', false);
+header('Pragma: no-cache');
+header('Connection: keep-alive');
+
+?>
